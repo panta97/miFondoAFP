d7c73a69805752ed22df145a8a99d02e12dab386
Roosevelt <panta_85.x@hotmail.com>
2018-10-11 00:35:52 -0500

    tweaks

--- Pages/ComisionFlujo.cshtml.cs
+++ Pages/ComisionFlujo.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace MiFondoAFP.Pages
 {
     public class ComisionFlujoModel : PageModel
     {
+
+        public string Mensaje { get; set; } = "Hola";
+
         public void OnGet()
         {
 
--- Pages/Estadistica.cshtml.cs
+++ Pages/Estadistica.cshtml.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace MiFondoAFP.Pages
 {
     public class EstadisticaModel : PageModel
     {
+        public string message { get; set; } = "HeloWrld";
         public void OnGet()
         {
-
         }
     }
 }
